7795b52f63dd53755f606e306fdb1c493075e32f
Marwa Yahia Galal <marwa.ygalal@gmail.com>
2018-11-28 17:34:36 +0200

    try to solve problem of error of server localhost.

--- server/src/index.js
+++ server/src/index.js
@@ -19,7 +19,7 @@ const server = new GraphQLServer({
     ...req,
     db: new Prisma({
       typeDefs: 'src/generated/prisma.graphql',
-      endpoint: 'http://localhost:4466',
+      endpoint: 'http://prisma:4466',
       secret: 'mysecret123',
       debug: true
     }),
